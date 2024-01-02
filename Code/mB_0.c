dad1868874dbf200af06f685f28daf69e40bb423
Aidan Brennan <89869586+Fuscior@users.noreply.github.com>
2024-01-02 14:04:13 +0000

    Update mB_0.c

--- Code/mB_0.c
+++ Code/mB_0.c
@@ -5,32 +5,41 @@
 #include "xil_printf.h"
 #include "xparameters.h"
 #include "xbasic_types.h"
+#include "xil_exception.h"	//isr
 
 
-//Base Address of the RTL_MULTIPLIER
-Xuint32 *baseaddr_p = (Xuint32 *) XPAR_DWC_V1_1_0_S00_AXI_BASEADDR;
+//Base Address of the dwc_V2
+Xuint32 *baseaddr_p = (Xuint32 *) XPAR_MYDWC_V2_0_S00_AXI_BASEADDR;
+
+volatile unsigned int *setReg = (unsigned int *)XPAR_MYDWC_V2_0_S00_AXI_BASEADDR;	//set register
+
+void myIsr(void *CallbackRef);
+void enable_ISR(void);
 
 int main()
 {
     init_platform();
+    enable_ISR();	//initilise ISR
 
     print("===================================================\n\r");
     print("MicoBlaze_0\n\r");
 
 
-	xil_printf("bass address is: 0x%08x \n\r", (Xuint32 *) XPAR_DWC_V1_1_0_S00_AXI_BASEADDR);
-	xil_printf("Performing a test of the DWC... \n\r");
-	// Write multiplier inputs to register 0
-	*(baseaddr_p + 0) = 0x00000003;
-	xil_printf("Wrote to register 0: 0x%08x \n\r", *(baseaddr_p + 0));
+	xil_printf("bass address is: 0x%08x \n\r", (Xuint32 *) XPAR_MYDWC_V2_0_S00_AXI_BASEADDR);
+	// Write inputs to register 1
+	*(baseaddr_p + 1) = 0x00000005;
+	setBit(setReg, 0);
+
+	xil_printf("Wrote to register 0: 0x%08x \n\r", *(baseaddr_p + 1));
 	// Read multiplier output from register 1
 
 
 	for(int i=0; i<99999999; i++){}
 	print("MicoBlaze_0\n\r");
-	xil_printf("Read from register 0: 0x%08x \n\r", *(baseaddr_p + 0));
-	xil_printf("Read from register 1: 0x%08x \n\r", *(baseaddr_p + 1));
-	xil_printf("Read from register 3: 0x%08x \n\r", *(baseaddr_p + 3));
+	xil_printf("Read from register 0 set reg: 0x%08x \n\r", *(baseaddr_p + 0));
+	xil_printf("Read from register 1 uP0: 0x%08x \n\r", *(baseaddr_p + 1));
+	xil_printf("Read from register 2 up1: 0x%08x \n\r", *(baseaddr_p + 2));
+	xil_printf("Read from register 5 isMatch: 0x%08x \n\r", *(baseaddr_p + 5));
 	xil_printf("End of test\n\n\r");
 
 
@@ -38,3 +47,53 @@ int main()
     cleanup_platform();
     return 0;
 }
+
+
+/*
+        4'h0   : reg_data_out <= slv_reg0; //set bits
+		4'h1   : reg_data_out <= slv_reg1; //uP0
+		4'h2   : reg_data_out <= slv_reg2; //uP1
+		4'h3   : reg_data_out <= slv_reg3;
+		4'h4   : reg_data_out <= slv_reg4;
+		4'h5   : reg_data_out <= isMatach_flag; //isMatch reg 5
+		4'h6   : reg_data_out <= slv_reg6;
+		4'h7   : reg_data_out <= slv_reg7;
+		4'h8   : reg_data_out <= slv_reg8;
+		4'h9   : reg_data_out <= slv_reg9;
+		default : reg_data_out <= 0;
+ */
+
+//--------------------------------------------------------------------------------------
+//-----set register---------------------------------------------------------------------
+ void setBit(unsigned int *setReg, int bitNumber){
+	// Read the current value
+	unsigned int value = *setReg;
+
+	// Set the desired bit
+	value |= (1 << bitNumber);
+
+	// Write the updated value back
+	*setReg = value;
+ }
+ //-----end set register-----------------------------------------------------------------
+ //--------------------------------------------------------------------------------------
+
+ //--------------------------------------------------------------------------------------
+ //-----enable interupt------------------------------------------------------------------
+ void enable_ISR(void){
+	// Connect ISR to the interrupt controller
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, myIsr, NULL);
+	Xil_ExceptionEnable();
+
+	// Enable MicroBlaze interrupts globally
+	microblaze_enable_interrupts();
+ }
+ //-----end enable interupt--------------------------------------------------------------
+ //--------------------------------------------------------------------------------------
+
+ void myIsr(void *CallbackRef) {
+     // ISR code
+     xil_printf("Interrupt occurred!\n");
+ }
+
+
