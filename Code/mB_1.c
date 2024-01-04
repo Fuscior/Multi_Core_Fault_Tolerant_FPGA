299dddcebd5b5534ba2e28189191223046291997
Aidan Brennan <89869586+Fuscior@users.noreply.github.com>
2024-01-04 15:05:35 +0000

    Update mB_1.c
    dwc1

--- Code/mB_1.c
+++ Code/mB_1.c
@@ -5,20 +5,20 @@
 #include "xil_printf.h"
 #include "xparameters.h"
 #include "xbasic_types.h"
-
+#include "xil_exception.h"	//isr
 
 //Base Address of the dwc_V2
-Xuint32 *baseaddr_p = (Xuint32 *) XPAR_MYDWC_V2_0_S00_AXI_BASEADDR;
+Xuint32 *baseaddr_p = (Xuint32 *) XPAR_DWC1_0_S00_AXI_BASEADDR;
 
-volatile unsigned int *setReg = (unsigned int *)XPAR_MYDWC_V2_0_S00_AXI_BASEADDR;	//set register
+volatile unsigned int *setReg = (unsigned int *)XPAR_DWC1_0_S00_AXI_BASEADDR;
+
+void enable_ISR(void);
+void myIsr(void *CallbackRef);
 
 int main()
 {
     init_platform();
 
-    //print("===================================================\n\r");
-    //print("MicoBlaze_1\n\r");
-    //print("===================================================\n\r");
 
 	*(baseaddr_p + 2) = 0x00000005;
 	setBit(setReg, 1);
@@ -27,6 +27,7 @@ int main()
     cleanup_platform();
     return 0;
 }
+
 //--------------------------------------------------------------------------------------
 //-----set register---------------------------------------------------------------------
  void setBit(unsigned int *setReg, int bitNumber){
@@ -41,3 +42,21 @@ int main()
  }
  //-----end set register-----------------------------------------------------------------
  //--------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------
+//-----enable interupt------------------------------------------------------------------
+void enable_ISR(void){
+	// Connect ISR to the interrupt controller
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, myIsr, NULL);
+	Xil_ExceptionEnable();
+
+	// Enable MicroBlaze interrupts globally
+	microblaze_enable_interrupts();
+}
+//-----end enable interupt--------------------------------------------------------------
+//--------------------------------------------------------------------------------------
+void myIsr(void *CallbackRef) {
+
+    //xil_printf("Interrupt occurred!\n");
+    //xil_printf("Read from register 5 isMatch in ISR: 0x%08x \n\r", *(baseaddr_p + 3));
+}
